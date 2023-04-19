313aa8d5cb385b7e51efc064d578ba7125fc01df
zzulanas <zzulanas@gmail.com>
2023-04-19 12:08:17 -0700

    final draft

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -4,16 +4,6 @@ const Overlay = () => {
   return (
     <div className={styles.overlay}>
       <iframe
-        src="https://open.spotify.com/embed/playlist/37i9dQZF1DX0Yxoavh5qJV?utm_source=generator"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allow="encrypted-media"
-        title="Spotify Playlist"
-        style={{ marginTop: "10px" }}
-        className={styles.musicPlaylist}
-      ></iframe>
-      <iframe
         className={styles.musicPlaylist}
         src="https://embed.music.apple.com/us/playlist/essential-christmas/pl.b0e04e25887741ea845e1d5c88397fd4"
         width="100%"
@@ -23,6 +13,16 @@ const Overlay = () => {
         style={{ width: "100%", height: "100%", border: "0" }}
         sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
         title="Apple Music Playlist"
+      ></iframe>
+      <iframe
+        src="https://open.spotify.com/embed/playlist/37i9dQZF1DX0Yxoavh5qJV?utm_source=generator"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allow="encrypted-media"
+        title="Spotify Playlist"
+        style={{ marginTop: "10px" }}
+        className={styles.musicPlaylist}
       ></iframe>
     </div>
   );
